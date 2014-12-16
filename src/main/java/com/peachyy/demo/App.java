476aace95529c06605b877b65ef84720f0450a78
peachyy <peachyy@sina.cn>
2014-12-16 23:22:56 +0800

    use java send email demosvcc

---
+++ src/main/java/com/peachyy/demo/App.java
@@ -0,0 +1,17 @@
+package com.peachyy.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+        System.out.println( "Hello World!" );
+    }
+    public void show(){
+    	System.out.println("fdgfdgfdg");
+    }
+}
---
+++ src/test/java/com/peachyy/demo/AppTest.java
@@ -0,0 +1,42 @@
+package com.peachyy.demo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+    	App c=new App();
+    	c.show();
+    	System.out.println("我要打断点1");
+    	System.out.println("我要打断点2");
+        assertTrue( true );
+    }
+}
